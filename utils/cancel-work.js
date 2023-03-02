5e92d146756801a20360d26049fc9a72ada10294
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2023-03-02 14:42:57 -0700

    Change to imitate release

--- utils/cancel-work.js
+++ utils/cancel-work.js
@@ -1,35 +1,18 @@
-import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 if (!github) {
   throw new Error("Module not found: github");
 }
 
-if (!core) {
-  throw new Error("Module not found: core");
-}
-
 async function main() {
   const {
-    repo: { owner, repo },
-  } = github.context;
+    GITHUB_TOKEN: token,
+    GITHUB_REPOSITORY_OWNER: owner,
+    GITHUB_REPOSITORY: fullRepoName,
+  } = process.env;
 
-  const { GITHUB_TOKEN } = process.env;
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-
-  console.log("RUN ACTION TO CHECK CANCELATION:");
-  await octokit.request(
-    "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
-    {
-      owner,
-      repo,
-      workflow_id: "cancelled.yml",
-      ref: "test",
-      inputs: {
-        version: "minor",
-      },
-    }
-  );
+  const octokit = github.getOctokit(token);
+  const [_, repo] = fullRepoName.split("/");
 
   await octokit.request(
     "GET /repos/{owner}/{repo}/actions/runs?status=in_progress",
@@ -37,7 +20,7 @@ async function main() {
   );
 
   setTimeout(async () => {
-    console.log("CANCEL ACTION:");
+    console.log("GET ALL ACTIVE ACTIONS:");
     const {
       data: { workflow_runs: allRuns },
     } = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
@@ -51,7 +34,7 @@ async function main() {
     );
     const runsToClean = allRuns.filter(
       ({ name, status }) =>
-        name === "Cancelled" && ["in_progress", "queued"].includes(status)
+        name === "Release" && ["in_progress", "queued"].includes(status)
     );
     console.log("RUNS TO BE CANCELLED:", runsToClean);
 
@@ -71,6 +54,20 @@ async function main() {
 
     console.log("Successfully done without any cancelations");
   }, 1000);
+
+  // console.log("RUN ACTION TO CHECK CANCELATION:");
+  // await octokit.request(
+  //   "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+  //   {
+  //     owner,
+  //     repo,
+  //     workflow_id: "release.yml",
+  //     ref: "test",
+  //     inputs: {
+  //       version: "minor",
+  //     },
+  //   }
+  // );
 }
 
 const runWorkflow = async (options) => {
@@ -93,6 +90,8 @@ const runWorkflow = async (options) => {
   }
 };
 
-main()
-  .then(() => core.info("Cancel Complete."))
-  .catch((e) => core.setFailed(e.message));
+main().catch((err) => {
+  console.error("Error:\n", err.message);
+  console.error("Stack:\n", err.stack);
+  process.exit(1);
+});
