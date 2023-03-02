457cd525e18daed55fc30149024707818893c43f
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2023-03-02 13:33:50 -0700

    add more functionality

--- utils/cancel-work.js
+++ utils/cancel-work.js
@@ -17,7 +17,7 @@ async function main() {
   const { GITHUB_TOKEN } = process.env;
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  console.log("RUN CANCELLED ACTION:");
+  console.log("RUN ACTION TO CHECK CANCELATION:");
   await octokit.request(
     "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
     {
@@ -34,32 +34,54 @@ async function main() {
   console.log("CANCEL ACTION:");
   const {
     data: { workflow_runs: allRuns },
-  } = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
-    owner,
-    repo,
-  });
-
-  const runsToCancel = allRuns.filter(
-    ({ name, status }) =>
-      name === "Cancelled" &&
-      ["queued", "waiting", "in-progress", "pending"].includes(status)
+  } = await octokit.request(
+    "GET /repos/{owner}/{repo}/actions/runs?state=in-progress",
+    {
+      owner,
+      repo,
+    }
   );
 
-  console.log(runsToCancel);
+  console.log("ALL RUNS:", allRuns);
+  const runsToClean = allRuns.filter((run) => run.name === "Cancelled");
+  console.log("RUNS TO BE CANCELLED:", runsToClean);
 
-  Promise.all(
-    runsToCancel.map((run) => {
-      return octokit.rest.actions.cancelWorkflowRun(
-        "GET /repos/{owner}/{repo}/actions/runs",
-        {
-          owner,
-          repo,
-          run_id: run.id,
-        }
-      );
-    })
-  );
+  if (runsToClean.length) {
+    console.log("cancelling....");
+    await octokit.rest.actions.cancelWorkflowRun(
+      "GET /repos/{owner}/{repo}/actions/runs",
+      {
+        owner,
+        repo,
+        run_id: runsToClean[0].id,
+      }
+    );
+    console.log("cancelation is done");
+    return;
+  }
+
+  console.log("Successfully done without any cancelations");
 }
+
+const runWorkflow = async (options) => {
+  try {
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+      {
+        owner,
+        repo,
+        workflow_id: "cancelled.yml",
+        ref: "test",
+        inputs: {
+          version: "minor",
+        },
+      }
+    );
+    console.log("Successfully completed dispatch job");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 main()
   .then(() => core.info("Cancel Complete."))
